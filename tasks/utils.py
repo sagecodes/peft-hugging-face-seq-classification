ac175e1e2c5cecb243257a692667eca3195efcc6
Sage <hello@sageelliott.com>
2025-02-19 17:18:29 -0800

    add image to base 64 to utils

---
+++ tasks/utils.py
@@ -0,0 +1,7 @@
+"""Utility functions for tasks."""
+
+
+# Convert images to base64 for embedding
+    def image_to_base64(image_path):
+        with open(image_path, "rb") as img_file:
+            return base64.b64encode(img_file.read()).decode("utf-8")
