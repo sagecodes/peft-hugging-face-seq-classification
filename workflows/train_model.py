a9e3394f19fe65983ec213f53501e2b7fceb97fd
Sage <hello@sageelliott.com>
2025-03-01 20:00:34 -0800

    start full pipeline of tasks in workflows

--- workflows/train_model.py
+++ workflows/train_model.py
@@ -1,6 +1,9 @@
+from tasks.data import download_data
 from tasks.model import download_sequence_class_model, fine_tune_model, fine_tune_model_lora
 import pandas as pd
+import os
 
+DATASET_NAME = "imdb" 
 MODEL_NAME = "distilbert-base-uncased"
 MODEL_DIR = "pre_trained_models\distilbert-base-uncased"
 data_paths = {
@@ -10,11 +13,36 @@ data_paths = {
 }
 
 
+def train_model_pipeline():
+    # check if the model is already downloaded
+    if not os.path.exists(MODEL_DIR):
+        download_sequence_class_model(MODEL_NAME)
+    
+    # check if the data is available
+    for path in data_paths.values():
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Data file {path} not found")
+
+
+    # Fine-tune the model
+    fine_tune_model(MODEL_DIR, data_paths)
+
+    # Fine-tune the model with LoRA
+    fine_tune_model_lora(
+        MODEL_DIR,
+        data_paths,
+        epochs=2,
+        lora_r=8,
+        lora_alpha=16,
+        lora_dropout=0.1,
+    )
 
 # download_sequence_class_model(MODEL_NAME)
 # fine_tune_model(MODEL_DIR, data_paths)
 
-# Fine-tune the model with LoR
+#--------------------------------
+# Fine-tune the model with LoRa 
+#--------------------------------
 fine_tune_model_lora(
     MODEL_DIR,
     data_paths,
@@ -24,6 +52,8 @@ fine_tune_model_lora(
     lora_dropout=0.1,
 )
 
+# python -m workflows.train_model fine_tune_model_lora
 
 # Run the script from the command line:
+
 # python -m workflows.train_model
